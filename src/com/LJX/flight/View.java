1befe73e7382f82f6b3c722e4805f39e0df8996a
6jiaxi0 <2104960955@qq.com>
2024-09-24 21:49:51 +0800

    Changes

--- src/com/LJX/flight/View.java
+++ src/com/LJX/flight/View.java
@@ -31,7 +31,7 @@ package com.LJX.flight;
  */
 
 
-public class Main {
+public class View {
     public static void main(String[] args) {
         new ReservationSystemProxy().startReservationSystem();
     }
--- src/com/LJX/flight/constant.java
+++ src/com/LJX/flight/constant.java
@@ -1,9 +1,17 @@
 package com.LJX.flight;
 
 public class constant {
+    /**
+     * 排数
+     */
     public final static int ROWS = 10;
+    /**
+     * 列数
+     */
     public final static int COLS = 4;
-
+    /**
+     * 特定于数组标志
+     */
     public final static int BLANK_SYMBOL = 0;
 
 }
